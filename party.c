c567932fd3f94dfa91e01dc22f39454aa1c32f60
KrishnaGunaShekar13 <gunagali13@gmail.com>
2026-04-09 09:51:51 +0530

    Add files via upload

---
+++ party.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int cigars,weekend;
+    printf("enter the number of cigars:\n");
+    scanf("%d",&cigars);
+    printf("enter 0 for not weekend\n enter 1 for weekend");
+scanf("%d,&weekend");
+if(weekend)
+{
+    if(cigars>=40)
+    {
+        printf("success");
+    }
+    else
+    {
+        printf("fail");
+}
+}
+else
+{
+    if(cigars>=40&&cigars<=60)
+{
+    printf("succcess");
+}else
+    {
+        printf("fail");
+    }
+    }}
